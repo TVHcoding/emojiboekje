3c7b709d18ab6beee2e6ffc68a1d2fb5c82a13e0
Tim Van heester <timvanheestercoding@gmail.com>
2018-07-09 11:37:35 +0200

    werkende code met 1 soort smiley

--- emoji boekje/ViewController.swift
+++ emoji boekje/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource , UITableViewDelegate {
 
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableview.dataSource = self
+        tableview.delegate = self
+        
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "😍"
+        return cell
+    }
+    
 }
 
